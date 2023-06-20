e6463ef417b004f4bc1291e908798c1923959898
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2023-06-20 19:15:24 +0700

    Application build

---
+++ dist/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.6.8/firebase-messaging.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCKuHqZJa78AeuNwDa9kUuNPB3N0O3Cc3k",
+    authDomain: "infus-monitor-frontend-e4021.firebaseapp.com",
+    projectId: "infus-monitor-frontend-e4021",
+    storageBucket: "infus-monitor-frontend-e4021.appspot.com",
+    messagingSenderId: "171370201164",
+    appId: "1:171370201164:web:83da90282956844dd034e9",
+    measurementId: "G-ENN2R9L803"
+});
+
+firebase.messaging();
